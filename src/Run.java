e0dafa046e4842ce3bb753c0da6d52b0919c361c
rsjavasolutions <stan.robert.1986@gmail.com>
2020-03-05 21:27:05 +0100

    Finished class Saving

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,6 @@
 import accounts.Account;
 import accounts.LifeLikeInMadrit;
+import accounts.Saving;
 import accounts.SuperCash;
 
 public class Run {
@@ -7,6 +8,13 @@ public class Run {
 
         Account superCash = new SuperCash();
         Account lifeLikeInMadrit = new LifeLikeInMadrit();
+        Account saving = new Saving();
+
+
+        saving.putMoney(100);
+        saving.putMoney(100);
+        saving.putMoney(100);
+        System.out.println(saving.checkStatus());
 
     }
 }
---
+++ src/accounts/Saving.java
@@ -0,0 +1,31 @@
+package accounts;
+
+public class Saving implements Account {
+
+    private double amount;
+
+    public Saving() {
+        amount = 10_000;
+    }
+
+    @Override
+    public void putMoney(double payment) {
+        payment *= 1.05;
+        amount += payment;
+    }
+
+    @Override
+    public void spendMoney(double payment) {
+        payment *= 1.1;
+        if (amount > payment){
+            amount -= payment;
+        } else {
+            System.out.println("Nie masz tyle kasy");
+        }
+    }
+
+    @Override
+    public String checkStatus() {
+        return amount + " PLN";
+    }
+}
